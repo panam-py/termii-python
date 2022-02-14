ec6a2f0d19f0b5288a5daefc4be5ef647a67e8d1
Hebron Praise <panampraisehebron@gmail.com>
2022-02-14 22:07:00 +0100

    modified client

--- termii/test.py
+++ termii/test.py
@@ -4,5 +4,5 @@ Phonebook_id = "5f74a4b2-84f9-446a-99ab-be902b6c932c"
 
 client = Client("TLP5Z7WVApOTkOtnRckecOGiOyNzY61x8A3LwIdwVNO8PAGhyrslhB6jKKxTHy")
 
-response = client.fetch_campaign_history('C620a709c7c512')
+response = client.fetch_history()
 print(response)
