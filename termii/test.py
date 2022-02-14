4bc5b83d820152072ef7d0572690e6eb663c1070
Hebron Praise <panampraisehebron@gmail.com>
2022-02-14 23:37:46 +0100

    added setup file

--- termii/test.py
+++ termii/test.py
@@ -4,5 +4,5 @@ Phonebook_id = "5f74a4b2-84f9-446a-99ab-be902b6c932c"
 
 client = Client("TLP5Z7WVApOTkOtnRckecOGiOyNzY61x8A3LwIdwVNO8PAGhyrslhB6jKKxTHy")
 
-response = client.fetch_history()
+response = client.add_contacts(r'C:\Users\Panam\Documents\termii-python\termii\contacts.csv', '234', 'text/csv', Phonebook_id)
 print(response)
